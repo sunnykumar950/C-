714c55bb89bd4ccc0803deed5974e01e6906410a
Sunny Kumar <sunnykumar91147@gmail.com>
2026-02-25 22:22:35 +0530

    Add files via upload

---
+++ name.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Sunny" << endl;
+    return 0;
+}
---
+++ sunny.cpp
@@ -0,0 +1,20 @@
+/*#include <iostream>
+using namespace std;
+ int  main()
+ {
+cout <<"sunny patel";
+return 0;
+}*/
+/*#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"sunny"<< endl<<"patel";
+    return 0;
+}*/
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"sunny"<<endl<<"patel";
+    return 0;
+}
